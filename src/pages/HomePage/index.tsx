f4b5bdb2acfbad02d73b21ab0b99c35feb955fff
Oghenetega Esedere <arcteggzz@gmail.com>
2023-04-10 23:54:59 +0100

    feat: implemented functionality for search panel toggle

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,15 +1,18 @@
+import { useContext } from "react";
 import styles from "./HomePage.module.scss";
 import AnimatedFadeInPage from "../../utils/AnimatedFadeInPage";
 import HomePageHeader from "./Components/HomePageHeader";
 import LatestTrends from "./Components/LatestTrends";
+import { AppContext } from "../../context/AppContext";
 
 const HomePage = () => {
+  const { searchHomePanelOpen } = useContext(AppContext);
   return (
     <>
       <AnimatedFadeInPage>
         <div className={styles.HomePage}>
           <HomePageHeader />
-          <LatestTrends />
+          {searchHomePanelOpen ? <LatestTrends /> : <></>}
         </div>
       </AnimatedFadeInPage>
     </>
